08946442135b85833eb82829ad49132c085564a0
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2018-09-08 13:25:07 +0300

    Add tests for cirlce's perameter and area

---
+++ Task1CSharpTests/CircleTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task1CSharp.Classes;
+using Xunit;
+
+namespace Task1CSharpTests
+{
+    public class CircleTests
+    {
+        private Circle circle;
+        private double radius;
+        private const double PI = 3.14;
+
+        public CircleTests()
+        {
+            radius = 4;
+            circle = new Circle { Center = new Point { X = 0, Y = 0 }, Radius = radius };
+        }
+
+        [Fact] 
+        public void PerimeterTest()
+        {
+            //Act
+            var result = circle.Perimeter();
+            var expected = 2 * PI * radius;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AreaTest()
+        {
+            //Act
+            var result = circle.Area();
+            var expected = PI * radius * radius;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
