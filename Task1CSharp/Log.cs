163f06872d5defa904b87cba5516cd20e63e581a
Maryan64 <maryangaming@gmail.com>
2018-09-20 16:47:33 +0300

    Log update (multithreading)

--- Task1CSharp/Log.cs
+++ Task1CSharp/Log.cs
@@ -7,16 +7,20 @@ namespace Task1CSharp
     public static class Log
     {
         private const string LOG_PATH = "../../log.txt";
+        private static object o = new object();
 
         public static void Message(string msg)
         {
-            string logMsg = $"{DateTime.Now.ToLongTimeString()} -> {msg}";
-            using (StreamWriter sw = new StreamWriter(LOG_PATH, true, Encoding.Default))
+            lock (o)
             {
-                sw.WriteLine(logMsg);
+                string logMsg = $"{DateTime.Now.ToLongTimeString()} -> {msg}";
+                using (StreamWriter sw = new StreamWriter(LOG_PATH, true, Encoding.Default))
+                {
+                    sw.WriteLine(logMsg);
+                }
+                
+                Console.WriteLine(logMsg);
             }
-
-            Console.WriteLine(logMsg);
         }
     }
 }
