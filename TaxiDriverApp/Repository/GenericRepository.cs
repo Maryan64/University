153818a29c5b4ddbbbb7400e22fcdc717468db9d
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2018-12-09 20:04:10 +0200

    Add using

--- TaxiDriverApp/Repository/GenericRepository.cs
+++ TaxiDriverApp/Repository/GenericRepository.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TaxiDriverApp.DataTypes;
 
 namespace TaxiDriverApp.Repository
 {
