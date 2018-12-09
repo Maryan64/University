2d9120123b81a0e88d4dc9b6fcf64925fa2e0ed8
Roman Myshchyshyn <roman1999.myshchyshyn@gmail.com>
2018-12-09 18:18:45 +0200

    Add Entity Framework, Pattern Repository

---
+++ TaxiDriverApp/DataTypes/DriverContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiDriverApp.DataTypes
+{
+    public class DriverContext : DbContext
+    {
+        public DbSet<TaxiDriver> Drivers { get; set; }
+        public DbSet<TaxiClient> Clients { get; set; }
+        public DbSet<TaxiOrder> Orders { get; set; }
+        public DriverContext() : base("name=DriverContext")
+        {
+        }
+    }
+}
