b42414eda2cee25267ed8fc59ab5517d2c474a99
andriy.datsyuk.2016@gmail.com <andriy.datsyuk.2016@gmail.com>
2018-12-10 15:35:35 +0200

    Create IUnitOfWork.cs

---
+++ TaxiDriverApp/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Entity_Framework__Repository__UnitOfWork.DataTypes;
+using Entity_Framework__Repository__UnitOfWork.Repository;
+
+namespace Entity_Framework__Repository__UnitOfWork.UnitOfWorkNS
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<TaxiClient> Clients { get; }
+        GenericRepository<TaxiDriver> Drivers { get; }
+        GenericRepository<TaxiOrder> Orders { get; }
+    }
+}
